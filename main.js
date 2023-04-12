414fce50cb7625e152a77f83463289cb597dfc35
Frodriks <frodriks@gmail.com>
2023-04-12 10:03:21 +0200

    Min första commit

---
+++ main.js
@@ -0,0 +1,22 @@
+/* Introduktion */
+
+//Hello World!
+
+console.log('Hello, World');
+
+//Variables
+let greeting = 'Hej från en vaiable';
+console.log(greeting);
+greeting = 3;
+console.log(greeting);
+
+greeting = "Hello!";
+const PI = 3.14;
+
+
+//Printing
+//Concatenation
+console.log(greeting + ' PI is: ' + PI);
+
+//Interpolation and templating literals
+console.log(`${greeting}, PI is: ${PI}`);
